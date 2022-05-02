49588f3bee70c2f240f552450974ad25fb94860c
riya-si <98996442+riya-si@users.noreply.github.com>
2022-05-02 16:57:23 -0700

    update

--- main.js
+++ main.js
@@ -27,6 +27,7 @@ function getBook() {
     } else if (randomBook == 'The Giver') {
         description = 'A book about a boy named Jonas who lives in a society free of crime and sadness and his path to uncover the secrets hidden from ordinary citizens.'
         tag = 'powers, dystopian, adventure'
+        
     } else if (randomBook == 'The Selection') {
         tag = 'romance, fairytale, suspense'
         description = 'A book about a boy named Jonas who lives in a society free of crime and sadness and his path to uncover the secrets hidden from ordinary citizens.'
@@ -39,6 +40,9 @@ function getBook() {
     } else if (randomBook == 'Golden Girl') {
         tag = 'fiction, family, love'
         description = 'A book that depicts the story of author Vivan who dies in a car crash which ultimately brings her loved ones closer together'
+    } else if (randomBook == 'One of Us is Lying') {
+        tag = 'suspense, thriller, murder-mystery'
+        description = 'A book that depicts the story of what happens when 5 people walk into detention and only 4 walk out'
     } 
     document.getElementById('description').innerHTML = description;
     document.getElementById('tag').innerHTML = tag;
