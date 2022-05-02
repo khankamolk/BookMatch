f30b83a451912519e417f247eaf10aeceb841750
riya-si <98996442+riya-si@users.noreply.github.com>
2022-05-02 16:53:46 -0700

    update

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ function randomNumber(num) {
 }
 
 function getBook() {
+    var description = ''
+    var tag = ''
     const dict = {
         0: 'The Giver',
         1: 'Harry Potter', 
@@ -19,6 +21,27 @@ function getBook() {
     const randomBook = dict[randomNumber(Object.keys(dict).length)];
     console.log(randomBook)
     document.getElementById('randomBook').innerHTML = randomBook;
+    if (randomBook == 'Harry Potter') {
+        description = 'A book about a boy who is a wizard.'
+        tag = 'magic, fantasy, adventure'
+    } else if (randomBook == 'The Giver') {
+        description = 'A book about a boy named Jonas who lives in a society free of crime and sadness and his path to uncover the secrets hidden from ordinary citizens.'
+        tag = 'powers, dystopian, adventure'
+    } else if (randomBook == 'The Selection') {
+        tag = 'romance, fairytale, suspense'
+        description = 'A book about a boy named Jonas who lives in a society free of crime and sadness and his path to uncover the secrets hidden from ordinary citizens.'
+    } else if (randomBook == 'The Fault in Our Stars') {
+        tag = 'emotional, romance, heartfelt'
+        description = 'A book about a girl named Hazel who joins a cancer support groups and eventually falls in love with a boy she meets through this group named Augustus Waters.'
+    } else if (randomBook == 'The Martian') {
+        tag = 'science-fiction, space, fact'
+        description = 'A book that details what happens when a mission to Mars goes awry and an astronaut is left to fend for himself in Mars using science and theory.'
+    } else if (randomBook == 'Golden Girl') {
+        tag = 'fiction, family, love'
+        description = 'A book that depicts the story of author Vivan who dies in a car crash which ultimately brings her loved ones closer together'
+    } 
+    document.getElementById('description').innerHTML = description;
+    document.getElementById('tag').innerHTML = tag;
    
 }
 generateButton.onclick = getBook;
